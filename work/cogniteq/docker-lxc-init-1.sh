e3a560a2686094c7a48b64306cda6fbc77dcc85b
vitovts <64224665+vitovts@users.noreply.github.com>
2020-08-25 14:02:01 +0300

    Create docker-lxc-init-1.sh

---
+++ work/cogniteq/docker-lxc-init-1.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+apt update && \
+apt install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     gnupg-agent\
+     software-properties-common -y && \
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add - && \
+apt-key fingerprint 0EBFCD88 && \
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable" && \
+apt update && \
+apt upgrade -y && \
+apt install docker-ce docker-ce-cli containerd.io -y && \
+docker run hello-world
+docker ps
+
+
+
+
